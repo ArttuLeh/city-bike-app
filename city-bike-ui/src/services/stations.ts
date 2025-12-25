590c7c837b3b4a96f96d1376643346003ffe66d7
ArttuLeh <lehtovaara.arttu@gmail.com>
2025-12-25 10:49:56 +0200

    comment removed

--- city-bike-ui/src/services/stations.ts
+++ city-bike-ui/src/services/stations.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { /*Station,*/ StationDetails, StationsResponse } from '../types'
+import { StationDetails, StationsResponse } from '../types'
 const apiUrl = process.env.REACT_APP_API_URL + '/api/stations'
 
 //axios fetch the data from backend
