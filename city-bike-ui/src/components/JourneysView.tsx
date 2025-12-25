1fb215363e3dc132bf0e811a800cb97abaea8418
ArttuLeh <lehtovaara.arttu@gmail.com>
2025-12-25 10:43:21 +0200

    loading icon added

--- city-bike-ui/src/components/JourneysView.tsx
+++ city-bike-ui/src/components/JourneysView.tsx
@@ -1,23 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { Box, TextField } from '@mui/material'
+import { Box, TextField, CircularProgress, } from '@mui/material'
 import { useEffect, useState, ChangeEvent } from 'react'
 import { initializeJourneys } from '../reducers/journeyReducer'
-import { toggleLoading } from '../reducers/loadingReducer'
 import SearchedJourneysData from './SearchedJourneysData'
 import JourneysData from './JourneysData'
-//import { Journey } from '../types'
 import { AppDispatch, RootState } from '../store'
-
-/*interface Props {
-  journeys: {
-    data: Journey[]
-    totalPages: number
-  }
-  sortOrder?: string
-  sortField?: string
-  loading?: boolean
-  handleSort: (value: string) => void
-}*/
 
 // component that handle which jorneys show, searched or all
 const JourneysList = () => {
@@ -27,10 +14,9 @@ const JourneysList = () => {
   const [sortOrder, setSortOrder] = useState<string>('') // state for sortorder (asc or desc)
   const [searchTerm, setSearchTerm] = useState<string>('') // state for searching value
   const journeys = useSelector(({ journeys } : RootState) => journeys) // get the journeys data from the store
-  
+  const isLoading = useSelector(({ loading } : RootState) => loading) // loading state for handling loading image
 
   useEffect(() => {
-    dispatch(toggleLoading(false))
     const delay = setTimeout(() => {
       // delay for searching
       dispatch(
@@ -91,23 +77,29 @@ const JourneysList = () => {
   return (
     <div>
       <h2>Journeys</h2>
-      <Box>
-        <TextField
-          id="search"
-          type="search"
-          label="Search departure station by name"
-          value={searchTerm}
-          onChange={handleSearch}
-          sx={{ width: 600 }}
+        <Box>
+          <TextField
+            id="search"
+            type="search"
+            label="Search departure station by name"
+            value={searchTerm}
+            onChange={handleSearch}
+            sx={{ width: 600 }}
+          />
+        </Box>
+      {!isLoading && journeys ? (
+        <JourneysData
+          journeys={journeys ?? { data: [], totalPages: 0 }}
+          sortOrder={sortOrder}
+          sortField={sortField}
+          handlePageChange={handlePageChange}
+          handleSort={handleSort}
         />
-      </Box>
-      <JourneysData
-        journeys={journeys ?? { data: [], totalPages: 0 }}
-        sortOrder={sortOrder}
-        sortField={sortField}
-        handlePageChange={handlePageChange}
-        handleSort={handleSort}
-      />
+      ) : (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <CircularProgress />
+        </Box>
+      )}
     </div>
   )
 }
