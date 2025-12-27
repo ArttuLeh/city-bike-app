b03b29a16157f72cebe197bf53b6f9788925c4b7
ArttuLeh <lehtovaara.arttu@gmail.com>
2025-12-27 21:36:55 +0200

    add station functionality added and some url update

--- city-bike-ui/src/services/journeys.ts
+++ city-bike-ui/src/services/journeys.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
 import { Journey, JourneyFormValues, JourneysResponse } from '../types'
-const apiUrl = process.env.REACT_APP_API_URL + '/api/journeys'
+const apiUrl = process.env.REACT_APP_API_URL// + '/api/journeys'
 
 //axios fetch the data from backend
 const getAll = async (currentPage: number, sortField: any, sortOrder: any, searchTerm: string) => {
   const { data } = await axios.get<JourneysResponse>(
-    `${apiUrl}?page=${currentPage}&sortField=${sortField}&sortOrder=${sortOrder}&search=${searchTerm}`
+    `${apiUrl}/api/journeys?page=${currentPage}&sortField=${sortField}&sortOrder=${sortOrder}&search=${searchTerm}`
   )
   console.log(data)
   return data
 }
 
 const create = async (content: JourneyFormValues) => {
-  const { data } = await axios.post<Journey>(apiUrl, content)
+  const { data } = await axios.post<Journey>(`${apiUrl}/api/journeys`, content)
   console.log("createNew",data)
   return data
 }
--- city-bike-ui/src/services/stations.ts
+++ city-bike-ui/src/services/stations.ts
@@ -1,20 +1,27 @@
 import axios from 'axios'
-import { StationDetails, StationsResponse } from '../types'
-const apiUrl = process.env.REACT_APP_API_URL + '/api/stations'
+import { StationDetails, StationsResponse, StationFormValues, Station } from '../types'
+const apiUrl = process.env.REACT_APP_API_URL// + '/api/stations'
 
 //axios fetch the data from backend
 const getAll = async (currentPage: number, searchTerm: string) => {
   const { data } = await axios.get<StationsResponse>(
-    `${apiUrl}?page=${currentPage}&search=${searchTerm}`
+    `${apiUrl}/api/stations?page=${currentPage}&search=${searchTerm}`
   )
   console.log("services", currentPage)
   return data
 }
 //fetch the specific station data from backend
 const getStation = async (id: number) => {
-  const { data } = await axios.get<StationDetails>(`${apiUrl}/${id}`)
+  const { data } = await axios.get<StationDetails>(`${apiUrl}/api/station/${id}`)
   console.log("services", data)
   return data
 }
+
+const create = async (content: StationFormValues) => {
+  const { data } = await axios.post<Station>(`${apiUrl}/api/stations`, content)
+  console.log("createNew",data)
+  return data
+}
+
 // eslint-disable-next-line
-export default { getAll, getStation }
+export default { getAll, getStation, create }
