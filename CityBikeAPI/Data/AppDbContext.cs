42368c80f36023e79cd4ea83eb1d5877de762255
ArttuLeh <lehtovaara.arttu@gmail.com>
2026-07-16 20:28:17 +0300

    added role-based authentication

--- CityBikeAPI/Data/AppDbContext.cs
+++ CityBikeAPI/Data/AppDbContext.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using CityBikeApi.Models;
 
 namespace CityBikeApi.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
